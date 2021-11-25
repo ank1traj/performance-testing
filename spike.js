dcf794b43fa76fdd51cc668b5fa754a57d6200e7
Ankit Raj <ankit.raj928@gmail.com>
2021-11-25 16:49:13 +0530

    Update spike.js

--- spike.js
+++ spike.js
@@ -8,8 +8,8 @@ export const options = {
   stages: [
     { duration: '10s', target: 100 }, // below normal load
     { duration: '1m', target: 100 },
-    { duration: '10s', target: 14000 }, // spike to 1400 users
-    { duration: '3m', target: 14000 }, // stay at 1400 for 3 minutes
+    { duration: '10s', target: 1400 }, // spike to 1400 users
+    { duration: '3m', target: 1400 }, // stay at 1400 for 3 minutes
     { duration: '10s', target: 100 }, // scale down. Recovery stage.
     { duration: '3m', target: 100 },
     { duration: '10s', target: 0 },
